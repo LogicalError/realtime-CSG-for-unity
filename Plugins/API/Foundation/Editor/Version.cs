9f25003116f429aede023ecbc8c83d9dc065d767
Daniel Cornelius <Kerfuffles@users.noreply.github.com>
2022-12-07 20:54:03 -0600

    Bump RCSG version to reflect current changes

--- Plugins/API/Foundation/Editor/Version.cs
+++ Plugins/API/Foundation/Editor/Version.cs
@@ -2,7 +2,7 @@ namespace RealtimeCSG.Foundation
 {
     internal static class Versioning
     {
-        public const string PluginVersion       = "1_572";
+        public const string PluginVersion       = "1_601";
         public const string PluginDLLVersion    = "1_559";
     }
 }
